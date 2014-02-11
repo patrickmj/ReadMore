f1a40d43e0860c6f2383ae98503408cfed419e01
patrickmj <patrickmjchnm@gmail.com>
2014-02-11 09:49:58 -0500

    Add full title to 'read more' link for a11y reasons
    Screen readers will read all the links, which would otherwise sound like "read more, read more read more read more"

--- ReadMorePlugin.php
+++ ReadMorePlugin.php
@@ -3,8 +3,8 @@
 class ReadMorePlugin extends Omeka_Plugin_AbstractPlugin
 {
     protected $_filters = array('filterReadMore' => array('Display', 'Item', 'Dublin Core', 'Description') );
-    
-    
+
+
     public function filterReadMore($text, $args)
     {
         if(isset($_GET['read_more']) && $_GET['read_more'] == true) {
@@ -12,8 +12,8 @@ class ReadMorePlugin extends Omeka_Plugin_AbstractPlugin
         }
         $record = $args['record'];
         $url = url("items/show/{$record->id}?read_more=true");
-        $link = "<a href='$url'>(read more)</a>";
-        $snippet = snippet($text, 0, 200, $link);        
+        $link = "<a href='$url'>(read more about " . metadata($record, array('Dublin Core', 'Title')) . " )</a>";
+        $snippet = snippet($text, 0, 200, $link);
         return $snippet;
     }
 }
